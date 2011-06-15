fc7db3cd9fcad7e9533fc51686103cbcf21977db
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2011-06-15 17:34:51 +0200

    Modified setup to work with python3 by using 2to3 fixers. This can be uploaded to pypi and get listed in the python3 compatible packages.

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
-from distutils.core import setup
-import mailer
+from setuptools import setup
+import sys
 
-setup (name='mailer',
-       version=mailer.__version__,
-       description=mailer.__description__,
-       author=mailer.__author__,
-       py_modules=["mailer"])
+extra = {}
+if sys.version_info >= (3,0):
+    extra.update( use_2to3 = True )
+
+setup (
+    name = 'mailer',
+    version = "0.7",
+    description = "A module to send email simply in Python",
+    author = "Ryan Ginstrom",
+    url = "https://bitbucket.org/ginstrom/mailer",
+    py_modules = ["mailer"],
+    keywords = ["email", "smtp"],
+    license = "MIT License",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    **extra
+)
